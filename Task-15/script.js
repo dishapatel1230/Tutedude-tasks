49b4b1c81867d53ef6ad52deaf90b40c9032b19e
dishapatel1230 <dishapatel3012@gmail.com>
2026-04-27 19:00:39 +0530

    update feedback

--- Task-15/script.js
+++ Task-15/script.js
@@ -3,6 +3,7 @@ const numbers = [10, 25, 7, 42, 19, 8, 33];
 
 // 1. Find maximum number (Arrow Function)
 const findMax = (arr) => {
+      if (arr.length === 0) return null;
     let max = arr[0];
     for(let  num of arr){
         if(num>max){
@@ -10,10 +11,12 @@ const findMax = (arr) => {
         }
     }
     return max;
-}
+};
 
 // 2. Calculate sum of all elements (Anonymous Function assigned to variable)
 const calculateSum = function(arr){
+      if (arr.length === 0) return 0;
+
     let sum = 0;
     arr.forEach(function(num){
         sum += num;
@@ -22,13 +25,15 @@ const calculateSum = function(arr){
 };
 // 3. Count number of odd numbers (Traditional Function)
 function countOddNumbers(arr){
+      if (arr.length === 0) return 0;
+
     let count = 0;
     for(let num of arr){
         if(num % 2 !==0){
             count++;
         }
     }
-    return count++;
+    return count;
 }
 
 //Display results;
@@ -36,3 +41,21 @@ console.log("Array:",numbers);
 console.log("Maximum number:",findMax(numbers));
 console.log("Sum of elements:",calculateSum(numbers));
 console.log("Cout of odd numbers:",countOddNumbers(numbers));
+
+/*test Array*/
+const testArrays = {
+  normal: [10, 25, 7, 42, 19, 8, 33],
+  empty: [],
+  single: [5],
+  allEven: [2, 4, 6, 8],
+  allOdd: [1, 3, 5, 7]
+};
+for (let key in testArrays) {
+  const arr = testArrays[key];
+
+  console.log(`\nTest Case: ${key}`);
+  console.log("Array:", arr);
+  console.log("Max:", findMax(arr));
+  console.log("Sum:", calculateSum(arr));
+  console.log("Odd Count:", countOddNumbers(arr));
+}
